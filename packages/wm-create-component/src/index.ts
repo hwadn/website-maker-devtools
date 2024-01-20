e48c6f3ff4b7557f806dc8d5041b88daa472cb6e
IamHuadong <1761339618@qq.com>
2024-01-20 15:38:22 +0800

    feat: download repo

--- packages/wm-create-component/src/index.ts
+++ packages/wm-create-component/src/index.ts
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
 import { program } from 'commander'
+import inquirer from 'inquirer'
+import downloadGitRepo from 'download-git-repo'
+
 import pjson from '../package.json'
 
 program.usage('<command>')
 program.version(pjson.version, '-v, --version')
 program.parse()
+
+inquirer.prompt([
+	{
+			name: 'componentName',
+			message: 'your component name: '
+	},
+	{
+			name: 'description',
+			message: 'description: ',
+			default: 'a wm component'
+	}
+]).then(answers => {
+	// 拿到信息参数
+	const { componentName, description } = answers
+	const beginTime = new Date().getTime()
+	console.log('start: jjj')
+	downloadGitRepo('github:hwadn/wm-component-template', `./${componentName}`, (err: any) => {
+		const time = (new Date().getTime() - beginTime) / 1000
+    console.log(err || `create project finish in ${time}s`)
+	})
+})
